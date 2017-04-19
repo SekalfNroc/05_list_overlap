8a36b78cbf394e6a3291ddd1fb8cd0ae2b81f6c1
Sekalf Nroc <sekalf.256@gmail.com>
2017-04-19 18:56:24 +0100

    1.0.0 Found Unique Intersection
    Through the magic of Ruby, found the intersection of all sets and
    filtered out duplicates.

--- main.rb
+++ main.rb
@@ -2,8 +2,11 @@
 
 sets = [
   [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89],
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+  [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 ]
 
-puts sets
+intersection = sets.inject :&  # Apply the & opperator bewteen all sets
+unique_intersection = intersection.uniq
+
+puts unique_intersection
 
